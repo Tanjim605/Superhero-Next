2d7007df2a0b497be8af5e4afe2b6bbd61e289d2
Tanjim605 <tanjimulislam2001@gmail.com>
2025-10-04 09:37:32 +0600

    next/Image added and css import fix

--- src/components/LoggedInUserInfo.tsx
+++ src/components/LoggedInUserInfo.tsx
@@ -1,5 +1,6 @@
 import { auth } from "@/auth";
 import SignOutBtnGithub from "./SignOutBtnGithub";
+import Image from "next/image";
 
 export default async function LoggedInUserInfo() {
   const session = await auth();
@@ -11,9 +12,9 @@ export default async function LoggedInUserInfo() {
       <div className="flex items-center justify-between w-auto mb-3 p-6 bg-amber-100 dark:bg-gray-400 rounded-2xl shadow-md">
         <div className= "flex gap-3 items-center">
           <div>
-            <img
-              src={session?.user?.image}
-              alt={session?.user?.name}
+            <Image
+              src={session?.user?.image ?? ""}
+              alt={session?.user?.name ?? ""}
               className="w-8 rounded-full"
             />
           </div>
